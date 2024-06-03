ce91cd5870bb2fec3327dd87f5716ab682ff3b32
giu 134 <giulioravazzolobass@gmail.com>
2024-06-03 16:24:59 +0200

    pagination

--- finance-backend/Repository/StockRepository.cs
+++ finance-backend/Repository/StockRepository.cs
@@ -46,7 +46,9 @@ namespace finance_backend.Repository
                 }
             }
 
-            return await stocks.ToListAsync();
+            var skipNumber = (query.PageNumber -1) * query.PageSize;
+
+            return await stocks.Skip(skipNumber).Take(query.PageSize).ToListAsync();
         }   
 
 
